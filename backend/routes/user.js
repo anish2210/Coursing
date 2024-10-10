f821727c6d97807392776a886aedf069dc5f8b12
anish2210 <anishjaiswal1220@gmail.com>
2024-10-11 00:33:24 +0530

    Implemented the signIn route completely

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,31 +3,34 @@ const userRouter = Router();
 const { userModel } = require("../db");
 const bcrypt = require("bcrypt");
 const { z } = require("zod");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const USER_JWT_SIGN = process.env.USER_JWT_SIGN;
 
 userRouter.post("/signup", async (req, res) => {
   try {
-      
     // Input validation using Zod
     const userInputValidation = z.object({
-          firstName: z.string().max(20),
-          lastName: z.string().max(20),
-          email: z.string().email(),
-          password: z.string().min(8)
-        });
+      firstName: z.string().max(20),
+      lastName: z.string().max(20),
+      email: z.string().email(),
+      password: z.string().min(8),
+    });
 
     const parsedUserData = userInputValidation.safeParse(req.body);
 
-    if(!parsedUserData.success){
-        console.log("User Input not valid");
-        res.json({
-            message: "User Input not valid",
-            error: parsedUserData.error
-        })
-        return
+    if (!parsedUserData.success) {
+      console.log("User Input not valid");
+      res.json({
+        message: "User Input not valid",
+        error: parsedUserData.error,
+      });
+      return;
     }
 
     const { firstName, lastName, email, password } = req.body;
-
 
     // Hashing password using bcryptjs
     const salt = await bcrypt.genSalt(10);
@@ -43,7 +46,6 @@ userRouter.post("/signup", async (req, res) => {
     res.json({
       message: "user account created successfully.",
     });
-
   } catch (error) {
     console.error("User not created: ", error);
     res.status(500).json({
@@ -52,12 +54,46 @@ userRouter.post("/signup", async (req, res) => {
   }
 });
 
-userRouter.post("/signin", (req, res) => {});
+userRouter.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await userModel.findOne({
+      email
+    });
+    if (!user) {
+      return res.json({
+        message:"invalid email or password!"
+      });
+    }
+    const isValidPassword = await bcrypt.compare(password, user.password);
+
+    if(!isValidPassword){
+      return res.json({
+        error: "invalid email or password!"
+      })
+    }
+
+    const token = jwt.sign(
+      {
+        id: user._id,
+      },
+      USER_JWT_SIGN);
+    
+    res.json({
+      token: token
+    })
+
+  } catch (error) {
+    console.error("Error: ", error);
+    res.json({
+      error: "Something is not good with this endpoint",
+    });
+  }
+});
 
 userRouter.get("/purchases", (req, res) => {
-  res.json({
-    message: "hi from signin",
-  });
+  res.json({});
 });
 
 module.exports = {
